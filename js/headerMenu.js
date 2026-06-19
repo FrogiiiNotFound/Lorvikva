ec8d52542be47d799273d2df061931d0ef1d0213
FrogiiiNotFound <samsung2903166@gmail.com>
2026-06-19 19:53:10 +0300

    fixes

--- js/headerMenu.js
+++ js/headerMenu.js
@@ -2,12 +2,15 @@ const body = document.querySelector('body');
 const menuIcon = document.querySelector('.icon-menu');
 const menuIcon2 = document.querySelector('.icon-menu2');
 const menuBody = document.querySelector('.menu__body');
+const headerContainer = document.querySelector('.header__container');
 
 if (menuIcon && menuBody && body) {
   menuIcon.addEventListener('click', (e) => {
     menuIcon.classList.toggle('_active');
     menuBody.classList.toggle('_active');
+    headerContainer.classList.toggle('_active');
     body.classList.toggle('_active');
+    body.classList.toggle('no-scroll');
   });
 }
 
@@ -15,7 +18,9 @@ if (menuIcon2) {
   menuIcon2.addEventListener('click', (e) => {
     menuIcon.classList.toggle('_active');
     menuIcon2.classList.toggle('_active');
+    headerContainer.classList.toggle('_active');
     menuBody.classList.toggle('_active');
     body.classList.toggle('_active');
+    body.classList.toggle('no-scroll');
   });
 }
