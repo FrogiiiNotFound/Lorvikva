1f6892fe5c5fc60c4ab39c609acb3cf200ff3035
Арсен <samsung2903166@gmail.com>
2026-06-23 15:04:17 +0300

    parallax changed

--- js/parallax.js
+++ js/parallax.js
@@ -1,21 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
   const parallaxEl = document.querySelector('.anim-item-main');
-  if (!parallaxEl) return;
+  const parallaxElMobile = document.querySelector('.anim-item-main2');
+  if (!parallaxEl && !parallaxElMobile) return;
 
-  const scrollIntensity = 0.3;
+  const scrollIntensity = 0.16;
+  const initialOffset = 200;
 
-  function initParallax() {
-    parallaxEl.classList.add('parallax-ready');
+  function createParallax(el) {
+    el.classList.add('parallax-ready');
 
     window.addEventListener(
       'scroll',
       () => {
         const scrollY = window.scrollY;
-        parallaxEl.style.transform = `translateY(${scrollY * scrollIntensity * -1}px)`;
+        el.style.transform = `translateY(${initialOffset + scrollY * scrollIntensity * -1}px)`;
       },
       { passive: true },
     );
   }
 
-  parallaxEl.addEventListener('animationend', initParallax, { once: true });
+  function initParallax(el) {
+    return () => createParallax(el);
+  }
+
+  if (parallaxEl) {
+    parallaxEl.addEventListener('animationend', initParallax(parallaxEl), { once: true });
+  }
+
+  if (parallaxElMobile) {
+    parallaxElMobile.addEventListener('animationend', initParallax(parallaxElMobile), {
+      once: true,
+    });
+  }
 });
